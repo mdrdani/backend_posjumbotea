3384d91f292572e94d79b52272ec7dded91ee6ab
mdrdani <sudokill3004@gmail.com>
2024-12-17 21:35:05 +0700

    add guard module product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe, UseInterceptors, UploadedFile, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @UseGuards(AuthGuard)
   @UsePipes(ValidationPipe)
   @Post()
   @UseInterceptors(FileInterceptor('image'))
@@ -18,16 +20,19 @@ export class ProductController {
     return await this.productService.create(createProductDto, file);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   async findAll() {
     return await this.productService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.productService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   @UseInterceptors(FileInterceptor('image'))
   async update(
@@ -38,6 +43,7 @@ export class ProductController {
     return await this.productService.update(+id, updateProductDto, file);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.productService.remove(+id);
